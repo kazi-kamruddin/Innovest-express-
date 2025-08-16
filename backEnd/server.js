d7dd81928dd813f37bc7c38ae3f06bdc5ab851b1
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-08-16 12:58:37 +0600

    cors issues (#2)

--- backEnd/server.js
+++ backEnd/server.js
@@ -11,7 +11,7 @@ const investorInfoRoutes = require("./routes/investorInfoRoutes");
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
